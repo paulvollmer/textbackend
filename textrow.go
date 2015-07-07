9c1f58494772d561793bbca69195682f0c383a8c
paulvollmer <paul.vollmer@fh-potsdam.de>
2015-07-07 12:47:18 +0200

    Created TextRow test, added GetLevelWhitespace and GetString funcs

--- textrow.go
+++ textrow.go
@@ -5,3 +5,17 @@ type TextRow struct {
 	Level int
 	Text  string
 }
+
+func (t *TextRow) GetLevelWhitespace(whitespace string) string {
+	tmp := ""
+	if t.Level > 0 {
+		for i := 0; i < t.Level; i++ {
+			tmp += whitespace
+		}
+	}
+	return tmp
+}
+
+func (t *TextRow) GetString(whitespace string) string {
+	return t.GetLevelWhitespace(whitespace) + t.Text
+}
---
+++ textrow_test.go
@@ -0,0 +1,20 @@
+package textbackend
+
+import (
+	"testing"
+)
+
+func TestGetString(t *testing.T) {
+	textRow := TextRow{}
+	textRow.Text = "hello world"
+	if textRow.GetString("\t") != "hello world" {
+		t.Error("GetString broken")
+	}
+
+	textRow2 := TextRow{}
+	textRow2.Level = 1
+	textRow2.Text = "hello world"
+	if textRow2.GetString("\t") != "\thello world" {
+		t.Error("GetString broken")
+	}
+}
