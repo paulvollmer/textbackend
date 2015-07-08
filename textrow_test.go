bbe997f331e94b62059c80e905ea4aebb91cc8b0
paulvollmer <paul.vollmer@fh-potsdam.de>
2015-07-08 16:14:14 +0200

    Added GetLevelWhitespace test

--- textrow_test.go
+++ textrow_test.go
@@ -4,17 +4,25 @@ import (
 	"testing"
 )
 
+func TestTextRow_GetLevelWhitespace(t *testing.T) {
+	textRow := TextRow{}
+	textRow.Level = 1
+	if textRow.GetLevelWhitespace("\t") != "\t" {
+		t.Error("GetLevelWhitespace failed")
+	}
+}
+
 func TestTextRow_GetString(t *testing.T) {
 	textRow := TextRow{}
 	textRow.Text = "hello world"
 	if textRow.GetString("\t") != "hello world" {
-		t.Error("GetString broken")
+		t.Error("GetString failed")
 	}
 
 	textRow2 := TextRow{}
 	textRow2.Level = 1
 	textRow2.Text = "hello world"
 	if textRow2.GetString("\t") != "\thello world" {
-		t.Error("GetString broken")
+		t.Error("GetString failed")
 	}
 }
